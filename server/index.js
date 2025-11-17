9926642595345a1e025e54addd553912c4c4ffb0
MoANsarMansoorie <moansarmansoorie@gmail.com>
2025-11-17 04:34:06 -0800

    server created

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+const PORT = process.env.PORT || 8080;
+
+// api end point
+app.get("/", (req, res) => {
+    res.send("Hello from server!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port http://localhost:${PORT}`)
+})
